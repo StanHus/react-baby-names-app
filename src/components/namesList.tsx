96bbd5b6696683b8ced5f2bddeb2b25a0baa1fe1
StanHus <guseletovS@gmail.com>
2021-09-01 14:22:18 +0100

    ex 1 done

--- src/components/namesList.tsx
+++ src/components/namesList.tsx
@@ -3,9 +3,11 @@ import namesData from "../names.json";
 import "../css/style.css";
 
 export default function NamesList(): JSX.Element {
-  return(
-  <body>
-    <div className="list">{namesData.map(NameEntry)}</div>
-  </body>
-  ) 
+  return (
+    <body>
+      <div className="list">
+        {namesData.sort((a, b) => a.name.localeCompare(b.name)).map(NameEntry)}
+      </div>
+    </body>
+  );
 }
